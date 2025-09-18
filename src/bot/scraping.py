52367ff1a89ee6a9ac44e6f82c2f4b9804a30ef4
PauloRugani <phrugany12020511@gmail.com>
2025-09-18 03:46:13 -0300

    feat: webscraping aus open 2025

---
+++ src/bot/scraping.py
@@ -0,0 +1,28 @@
+from playwright.sync_api import Playwright, sync_playwright
+import csv
+
+def run(playwright: Playwright) -> list[tuple[str, str]]:
+    try:
+        index = 1
+        games = []
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("https://www.tennisabstract.com/cgi-bin/tourney.cgi?t=2025-580/Australian-Open")
+        while True:
+            player_0 = page.locator(f'//*[@id="singles-results"]/tbody/tr[{index}]/td[3]/a[1]').first.inner_text()
+            player_1 = page.locator(f'//*[@id="singles-results"]/tbody/tr[{index}]/td[6]/a[1]').first.inner_text()
+            games.append((player_0, player_1))
+            index += 1
+    except Exception:
+        with open(r"data\external\australia_open_matches.csv", "w", newline='', encoding="utf-8") as f:
+            writer = csv.writer(f)
+            writer.writerow(["player_0", "player_1"])
+            writer.writerows(games)
+        return games
+    finally:
+        context.close()
+        browser.close()
+
+with sync_playwright() as playwright:
+    run(playwright)
