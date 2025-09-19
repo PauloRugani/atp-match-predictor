be5d6f8327353d66c37c888c71b9e030f9eaa399
PauloRugani <phrugany12020511@gmail.com>
2025-09-19 16:49:55 -0300

    refactor: adjust to new dataset

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from src.controller.main import predict_winner
 from typing import List, Tuple
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 import csv
 
 def run() -> Tuple[List[dict], float]:
     ao_open_matches = pd.read_csv(r'data\external\australia_open_matches.csv', sep=',', encoding='utf-8')
+    correct_result = ao_open_matches['winner']
+    print(ao_open_matches['winner'].value_counts())
     results = []
     model_result = []
-    correct = 0
     for _, row in ao_open_matches.iterrows():
         result = predict_winner(row['player_0'], row['player_1'], 5, 'HARD')
 
         if result == 0:
             winner = row['player_0']
             model_result.append(0)
-            correct += 1
         elif result == 1:
             winner = row['player_1']
             model_result.append(1)
@@ -29,16 +29,17 @@ def run() -> Tuple[List[dict], float]:
             'player_0': row['player_0'],
             'player_1': row['player_1'],
             'predicted_winner': winner,
-            'real_winner': row['player_0'] # player_0 is the winner
+            'real_winner': row[f'player_{row["winner"]}'] 
         })
-    accuracy = round((correct / len(ao_open_matches)), 2)
-    return results, accuracy, model_result
+    return results, correct_result, model_result
 
-ao_predict_result, accuracy, model_result = run()
-print(f'Accuracy: {accuracy * 100}%')
+ao_predict_result, correct_result, model_result = run()
 
-matrix = confusion_matrix(model_result, [0 for _ in range(1, 128)])
+accuracy = accuracy_score(correct_result, model_result)
+print(f'Accuracy: {round((accuracy * 100), 2)}%')
+
 plt.figure()
+matrix = confusion_matrix(correct_result, model_result)
 sns.heatmap(matrix, annot=True, fmt='d')
 plt.show()
 
