6012e80e45f727524c1bb223745096956cc8a780
PauloRugani <phrugany12020511@gmail.com>
2025-09-19 16:48:26 -0300

    refactor: randomized players order

--- src/bot/scraping.py
+++ src/bot/scraping.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import Playwright, sync_playwright
+from random import random
 import csv
 
 def run(playwright: Playwright) -> list[tuple[str, str]]:
@@ -12,12 +13,15 @@ def run(playwright: Playwright) -> list[tuple[str, str]]:
         while True:
             player_0 = page.locator(f'//*[@id="singles-results"]/tbody/tr[{index}]/td[3]/a[1]').first.inner_text()
             player_1 = page.locator(f'//*[@id="singles-results"]/tbody/tr[{index}]/td[6]/a[1]').first.inner_text()
-            games.append((player_0, player_1))
+            if random() > 0.5:
+                games.append((player_0, player_1, 0))
+            else:
+                games.append((player_1, player_0, 1))
             index += 1
     except Exception:
         with open(r"data\external\australia_open_matches.csv", "w", newline='', encoding="utf-8") as f:
             writer = csv.writer(f)
-            writer.writerow(["player_0", "player_1"])
+            writer.writerow(["player_0", "player_1", "winner"])
             writer.writerows(games)
         return games
     finally:
